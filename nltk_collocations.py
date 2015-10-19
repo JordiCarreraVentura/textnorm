ab8307fe29c03355a813e0c91520ec644ebbba3d
JordiCarreraVentura <jordi@immention.com>
2015-10-19 09:27:24 +0100

    Updated experimental NLTK code

--- nltk_collocations.py
+++ nltk_collocations.py
@@ -7,20 +7,19 @@ ROOT = './'
 PATH = 'essays.txt'
 
 corpus = reader(ROOT, [PATH])
-text = nltk.Text(corpus.words())
+words = nltk.Text([w.lower() for w in corpus.words()])
 
 bigram_measures = nltk.collocations.BigramAssocMeasures()
 trigram_measures = nltk.collocations.TrigramAssocMeasures()
 
 #	too rare; top results have a single occurrence
-finder2 = BigramCollocationFinder.from_words(nltk.Text(corpus.words()))
-finder3 = TrigramCollocationFinder.from_words(nltk.Text(corpus.words()))
+finder2 = BigramCollocationFinder.from_words(words)
+finder3 = TrigramCollocationFinder.from_words(words)
 
 
 finder2.apply_freq_filter(5)
 finder3.apply_freq_filter(5)
 print finder2.nbest(bigram_measures.pmi, 150)[-10:]
-# print finder2.nbest(bigram_measures.pmi, 500)[-10:]
+print finder2.nbest(bigram_measures.pmi, 500)[-10:]
 # print finder2.nbest(bigram_measures.pmi, 100000000000)[-20:]
-
-print finder3.nbest(trigram_measures.pmi, 100000000000)[-20:]
+# print finder3.nbest(trigram_measures.pmi, 100000000000)[-20:]
